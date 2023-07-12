db3a67292fd28b16a85ab41e85c8ee6be5751fd5
Matheus <matheus.rodrigues2010@hotmail.com>
2023-07-12 08:38:25 -0300

    login/logout user

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,6 +1,7 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
+  if (req.session.user) return res.render('login-logado.ejs');
   res.render('login.ejs');
 };
 
@@ -25,4 +26,33 @@ exports.register = async (req, res) => {
     console.log(e);
     return res.render('404.ejs');
   }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(() => {
+        return res.redirect('/login/index');
+      });
+      return;
+    }
+
+    req.flash('success', 'Seu usuário foi logado com sucesso.');
+    req.session.user = login.user;
+    req.session.save(() => {
+      return res.redirect('/login/index');
+    });
+  } catch (e) {
+    console.log(e);
+    return res.render('404.ejs');
+  }
+};
+
+exports.logout = (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
 };
